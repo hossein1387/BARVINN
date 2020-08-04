41e7a005fdd9d78417bd4de46fab9c526d6c53e9
hossein1387 <m.h.askari.hemmat@gmail.com>
2020-08-03 22:10:47 -0400

    links to pito files

--- csrc/common/link.sh
+++
@@ -1,7 +0,0 @@
-rm -rf Makefile.h
-ln -fs ../common/link.ld 
-ln -fs ../common/riscv_test.h 
-ln -fs ../common/Makefile
-ln -fs ../common/makehex.py 
-ln -fs ../common/riscv_hex_to_bin.py 
-ln -fs ../common/test_macros.h 
