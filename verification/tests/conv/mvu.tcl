17aa1c23e778f706834121139d400cdd07770831
hossein1387 <m.h.askari.hemmat@gmail.com>
2020-10-30 23:49:55 -0400

    testing conv in systemverilog

---
+++ verification/tests/conv/mvu.tcl
@@ -0,0 +1,21 @@
+add_wave_group mvu
+    add_wave -into mvu {{/conv_tester/accelerator/mvu/clk}}
+    add_wave_group -into mvu mvu0
+        add_wave_group -into mvu0 input_ram
+            add_wave -into input_ram {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /\bankarray[0].db /b/inst/\native_mem_module.blk_mem_gen_v8_4_3_inst /memory}}
+        add_wave_group -into mvu0 output_ram
+            add_wave -into output_ram {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /\bankarray[1].db /b/inst/\native_mem_module.blk_mem_gen_v8_4_3_inst /memory}}
+        add_wave_group -into mvu0 pipeline
+            add_wave -into pipeline {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /core_data}}
+            add_wave -into pipeline {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /core_out}}
+            add_wave -into pipeline {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /shacc_out}}
+            add_wave -into pipeline {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /scaler_out}}
+            add_wave -into pipeline {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /pool_out}}
+            add_wave -into pipeline {{/conv_tester/accelerator/mvu/\mvuarray[0].mvunit /quant_out}}
+        add_wave_group -into mvu0 configs
+            add_wave -into configs {{/conv_tester/accelerator/mvu/iprecision_q}}
+            add_wave -into configs {{/conv_tester/accelerator/mvu/wprecision_q}}
+            add_wave -into configs {{/conv_tester/accelerator/mvu/oprecision_q}}
+            add_wave -into configs {{/conv_tester/accelerator/mvu/quant_msbidx_q}}
+            add_wave -into configs {{/conv_tester/accelerator/mvu/countdown_q}}
+set_property display_limit 40000000 [current_wave_config]
