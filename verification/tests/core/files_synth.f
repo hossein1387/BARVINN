3288c6009a4ddf9c00e465dd21a31205a46a36d9
hossein1387 <m.h.askari.hemmat@gmail.com>
2021-05-07 11:03:12 -0400

    base test for barvinn

---
+++ verification/tests/core/files_synth.f
@@ -0,0 +1,6 @@
+/local/xilinx/Vivado/2019.1/ids_lite/ISE/verilog/src/glbl.v
+../../../vsrc/rv32_defines.svh
+../../lib/utils/utils.sv
+../../lib/utils/rv32_utils.sv
+../../../synthesis/pito_barreled/rv32_core_synth.v
+./core_tester.sv
